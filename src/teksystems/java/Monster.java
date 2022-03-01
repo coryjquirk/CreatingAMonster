4916d6a2770e07ac0bd22989a60a6a14de68ad9b
Cory Quirk <coryjquirk@gmail.com>
2022-03-01 13:12:51 -0600

    initial commit

---
+++ src/teksystems/java/Monster.java
@@ -0,0 +1,17 @@
+package teksystems.java;
+
+// Instructions:
+// https://drive.google.com/file/d/1Xnl0R38R8qeAYvZXix6Hv2vjm3ijdC28/view
+
+public class Monster {
+    //superclass
+    public String name;
+    public Monster(String monsterName){
+        name = monsterName;
+    }
+    //need an attack method for each monster type?
+    //types: water, fire, stone
+
+    //define a method for a monster "without a type"
+    //this method can be inherited and further specified in subclasses
+}
